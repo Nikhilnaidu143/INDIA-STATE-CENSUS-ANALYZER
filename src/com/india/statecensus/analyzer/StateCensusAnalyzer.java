a9ec7a468a9a7e54f1fb43ba5bf35e7dd0f2c5a5
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-20 14:13:53 +0530

    START:- Displayed Welcome Message.

---
+++ src/com/india/statecensus/analyzer/StateCensusAnalyzer.java
@@ -0,0 +1,8 @@
+package com.india.statecensus.analyzer;
+
+public class StateCensusAnalyzer {
+	public static void main(String[] args) {
+		/** Displayed Welcome message. **/
+		System.out.println("-----------WELCOME-TO-INDIA-STATE-CENSUS-ANALYZER-----------\n");
+	}
+}
